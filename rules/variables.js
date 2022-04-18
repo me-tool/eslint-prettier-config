4349e355da95976ccab3261fa41060d067391c1a
justin.xie <>
2022-04-18 14:40:19 +0800

    fix: no-unused-vars

--- rules/variables.js
+++ rules/variables.js
@@ -26,5 +26,16 @@ module.exports = {
     'no-undef-init': 'error', // 变量初始化时不能直接给它赋值为 undefined
     'no-undefined': 'off', // 不能使用 undefined
     'no-use-before-define': ['error', { functions: true, classes: true, variables: true }], // 禁止在变量定义之前使用它们
+    /**
+     * 禁止出现未使用过的变量
+     * 1. vars 默认值，检查所有包括全局变量
+     * 2. args 默认值，在一个变量使用过之后，在他之前的不做检查之后仍会检查
+     * 3. ignoreRestSiblings 默认值，用于进行属性 omit 和解构操作
+     * 4. argsIgnorePattern 自定义，以下划线开头的不错检查
+     */
+    'no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: true, argsIgnorePattern: '^_' },
+    ],
   },
 };
