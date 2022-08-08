876a69164aa0f2960b25b3734b96fb7820b3da6a
Justin.xie <minghao.xie@foxmail.com>
2022-08-08 20:49:23 +0800

    update, off no-extra-parens

--- rules/possible-errors.js
+++ rules/possible-errors.js
@@ -6,7 +6,15 @@ module.exports = {
   rules: {
     'no-await-in-loop': 'error', // 禁止在循环中出现 await
     'no-console': 'off', // 禁止使用console
-    'no-extra-parens': 'error', // 禁止非必要的括号
+    // disallow unnecessary parentheses
+    // https://eslint.org/docs/rules/no-extra-parens
+    'no-extra-parens': ['off', 'all', {
+      conditionalAssign: true,
+      nestedBinaryExpressions: false,
+      returnAssign: false,
+      ignoreJSX: 'all', // delegate to eslint-plugin-react
+      enforceForArrowConditionals: false,
+    }],
     'no-template-curly-in-string': 'error', // 禁止在常规字符串中出现模板字面量占位符语法
   },
 };
