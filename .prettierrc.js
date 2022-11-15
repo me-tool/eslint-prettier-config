6623ca4793cce8b2fddc6b3000359181386db28f
ext.xieminghao <ext.xieminghao@jd.com>
2022-11-15 10:56:41 +0800

    update prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
   arrowParens: 'always', // 箭头函数如果只有一个参数，添加括号
   endOfLine: 'lf', // 结束行形式
   bracketSpacing: true, // 是否保留对象内侧两端的空格，比如 { foo: bar } 和 {foo:bar} 的区别
-  jsxBracketSameLine: true, // 多属性html标签的‘>’折行放置
+  bracketSameLine: true,
   printWidth: 100,
   semi: true,
   quoteProps: 'as-needed', // 对象中的属性使用引号，"as-needed" 只对需要的属性加引号(默认值)
@@ -18,4 +18,5 @@ module.exports = {
   requirePragma: false, // 无需顶部注释即可格式化
   insertPragma: false, // 在已被 prettier 格式化的文件顶部加上标注
   vueIndentScriptAndStyle: true, // 是否对 Vue 文件中 <script> 和 <style> 标签内的代码应用缩进
+  htmlWhitespaceSensitivity: 'ignore', // 空格敏感 https://prettier.io/blog/2018/11/07/1.15.0.html#whitespace-sensitive-formatting
 };
